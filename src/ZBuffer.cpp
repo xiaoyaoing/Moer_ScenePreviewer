a0c124719cd14fadacb9e72f490e1bc9abecbd5f
Rickhu <rickhu123@outlook.com>
2023-10-08 23:32:40 +0800

    feat: add ZBuffer

---
+++ src/ZBuffer.cpp
@@ -0,0 +1,22 @@
+#include "ZBuffer.h"
+
+ZBuffer::ZBuffer(size_t _width, size_t _height)
+    : width(_width), height(_height) {
+   data.reserve(_width * _height);
+}
+
+float ZBuffer::getPixel(size_t x, size_t y) {
+   if (x > width || y > height) {
+      throw std::out_of_range("Out of Image's range");
+   }
+   return data[y * width + x];
+}
+void ZBuffer::setPixel(float x, float y, float depth) {
+   if (x > width || y > height) {
+      throw std::out_of_range("Out of Image's range");
+   }
+   size_t index = static_cast<size_t>(y * width + x);
+   if (depth < data[index]) {
+      data[index] = depth;
+   }
+}
